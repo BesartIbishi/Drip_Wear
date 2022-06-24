8e9d6f3e7c00ba377b9ac9adf2d603bb5fccb828
Besart Ibishi <bi47125@ubt-uni.net>
2022-06-24 11:50:07 +0200

    added basic files for theme and enqueued stylesheets

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Drip Wear
+ */
+
+?>
+
+
+<footer>
+			<section class="footer-widgets">Footer Widgets</section>
+			<section class="copyright">Copyright</section>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Drip Wear
+ */
+
+?>
+
+
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+
+    <!-- Kto e bejne resposive site-in -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+</head>
+<body <?php body_class(); ?>>
+	<div id="page" class="site">
+		<header>
+			<section class="search">Search</section>
+			<section class="top-bar">
+				<div class="brand">Logo</div>
+				<div class="second-column">
+					<div class="acount">Account</div>
+					<nav class="main-menu">Menu</nav>
+				</div>
+			</section>
+		</header>
