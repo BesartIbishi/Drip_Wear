2282a42407b0ab7713126a3e9857d410afce8e44
rilindtasholli <rilindtasholli@gmail.com>
2022-06-29 17:17:09 +0200

    added shopping cart, account page(login/register), custom logo and custom coypright text field

--- functions.php
+++ functions.php
@@ -10,7 +10,8 @@
  // Register Custom Navigation Walker
 require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 
-
+ // Register Custom Navigation Walker
+ require_once get_template_directory() . '/inc/customizer.php';
  function drip_wear_scripts(){
 
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri(  ).'/inc/bootstrap.min.js', array('jquery'), '4.3.1', true );
@@ -51,6 +52,13 @@ require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 	add_theme_support( 'wc-product-gallery-lightbox' );
 	add_theme_support( 'wc-product-gallery-slider' );
 
+	add_theme_support( 'custom-logo', array(
+		'height'		=> 85,
+		'width'			=> 160,
+		'flex-height'	=> true,
+		'flex-width'	=> true
+	) );
+
 	if ( ! isset( $content_width ) ) {
 		$content_width = 600;
 	}				
@@ -60,4 +68,21 @@ add_action( 'after_setup_theme', 'drip_wear_config', 0 );
 
 if( class_exists('woocommerce')){
 	require get_template_directory() . '/inc/wc-modifications.php';
+}
+
+
+//Show cart contents / total Ajax
+ 
+add_filter( 'woocommerce_add_to_cart_fragments', 'fancy_lab_woocommerce_header_add_to_cart_fragment' );
+
+function fancy_lab_woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+	<?php
+	$fragments['span.items'] = ob_get_clean();
+	return $fragments;
 }
